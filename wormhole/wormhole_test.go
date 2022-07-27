aae8ac9cfea29839b0badf8841467f5f5a2ee91d
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2022-07-27 18:31:46 +0530

    dilation: a simple test for dilation capability negotiation

--- wormhole/wormhole_test.go
+++ wormhole/wormhole_test.go
@@ -1231,7 +1231,7 @@ func TestWormholeFileTransportSendRecvViaWSRelayServer(t *testing.T) {
 	}
 }
 
-func TestVersionsMsgMarshalUnmarshal(t *testing.T) {
+func TestDilationVersionsMsgMarshalUnmarshal(t *testing.T) {
 	m := appVersionsMsg{[]string{"1"}}
 	jMsg, err := json.Marshal(m)
 	if err != nil {
@@ -1246,5 +1246,15 @@ func TestVersionsMsgMarshalUnmarshal(t *testing.T) {
 
 	if len(dm.CanDilate) != 1 || dm.CanDilate[0] != m.CanDilate[0] {
 		t.Fatalf("unserialized value does not match the original")
+	}
+}
+
+// test dilation capability exchange
+func TestDilationCapabilityNegotiation(t *testing.T) {
+	ctx := context.Background()
+
+	cc0 := newClientProtocol(ctx, nil, "foobar0", "dilateTest")
+	if !cc0.areBothSidesDilationCapable([]string{"1"}) {
+		t.Fatalf("expected the dilation versions to match")
 	}
 }
