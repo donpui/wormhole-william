4a6ad02d207ca9d19be282edecb58687a7057741
Justus Tumacder <JustusFT@gmail.com>
2022-11-17 23:59:42 +0800

    Fix wasm bindings for go 1.19 (#92)
    There was some changes to the `js` package between go 1.16 and 1.19.
    The `Wrapper` interface was removed in go 1.18 so the js side is not
    able to use the `Promise` type. To fix this, I changed the function to
    return a `js.Value` directly.

--- wasm/promise.go
+++ wasm/promise.go
@@ -7,25 +7,18 @@ import (
 	"syscall/js"
 )
 
-type Promise struct {
-	jsValue js.Value
-}
-
 type ResolveFn = func(interface{})
 type RejectFn = func(error)
 type PromiseFn = func(ResolveFn, RejectFn)
 
-func NewPromise(fn PromiseFn) *Promise {
+func NewPromise(fn PromiseFn) js.Value {
 	constructor := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		// TODO: error handling!!!
-		obj := js.Global().Get("Object").New()
-		obj.Set("resolve", args[0])
-		obj.Set("reject", args[1])
 		resolve := func(val interface{}) {
-			obj.Call("resolve", val)
+			args[0].Invoke(val)
 		}
 		reject := func(err error) {
-			obj.Call("reject", err.Error())
+			args[1].Invoke(err.Error())
 		}
 
 		go func() {
@@ -35,11 +28,5 @@ func NewPromise(fn PromiseFn) *Promise {
 	})
 	jsPromise := js.Global().Get("Promise").New(constructor)
 	constructor.Release()
-	return &Promise{
-		jsValue: jsPromise,
-	}
-}
-
-func (p *Promise) JSValue() js.Value {
-	return p.jsValue
+	return jsPromise
 }
